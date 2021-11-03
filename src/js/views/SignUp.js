5a79d5d494bbe36910292d3b2d264f3fc0217626
gars150387 <gar.santeliz@gmail.com>
2021-11-03 11:42:21 +0000

    sign up component with navigation

--- src/js/views/SignUp.js
+++ src/js/views/SignUp.js
@@ -1,24 +1,40 @@
 import React from "react";
-import { Container, Col } from "react-bootstrap";
+import { Container, Col, Row, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
 	return (
 		<Container>
-			<Col>
-				<input className="m-auto" type="text" placeholder="First Name" />
-			</Col>
-			<Col>
-				<input className="m-auto" type="text" placeholder="Last Name" />
-			</Col>
-			<Col>
-				<input className="m-auto" type="email" placeholder="Email" />
-			</Col>
-			<Col>
-				<input className="m-auto" type="password" placeholder="Password" />
-			</Col>
-			<Col>
-				<input className="m-auto" type="confirmationPassword" placeholder="Confirmation Password" />
-			</Col>
+			<Row md={10}>
+				<Col>
+					<input className="m-auto" type="text" placeholder="First Name" />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<input className="m-auto" type="text" placeholder="Last Name" />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<input className="m-auto" type="email" placeholder="Email" />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<input className="m-auto" type="password" placeholder="Password" />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<input className="m-auto" type="confirmationPassword" placeholder="Confirmation Password" />
+				</Col>
+			</Row>
+			<Link to="/">
+				<Form.Text className="text-start" style={{ marginLeft: "10px" }}>
+					If you do have an account. please click this link to sign in.
+				</Form.Text>
+			</Link>
 		</Container>
 	);
 };
