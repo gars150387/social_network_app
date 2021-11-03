f99f5ea6787ff1bc723ef6c6db2e8626d7b817e5
gars150387 <gar.santeliz@gmail.com>
2021-11-03 13:06:25 +0000

    style sign up component

--- src/js/views/SignUp.js
+++ src/js/views/SignUp.js
@@ -1,39 +1,52 @@
 import React from "react";
-import { Container, Col, Row, Form } from "react-bootstrap";
+import { Container, Col, Row, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const SignUp = () => {
 	return (
-		<Container style={{ width: "400px", height: "600px", border: "solid black 1px", marginTop: "20px" }}>
+		<Container
+			style={{
+				width: "400px",
+				height: "400px",
+				border: "solid black 1px",
+				marginTop: "20px",
+				borderRadius: "rounded"
+			}}>
 			<Row md={10}>
 				<Col className="w-100 h-15">
-					<input className="m-auto mt-3 mb-3  w-100 h-15" type="text" placeholder="First Name" />
+					<input className="m-auto mt-3 mb-3  w-100 h-15 rounded" type="text" placeholder="First Name" />
 				</Col>
 			</Row>
 			<Row>
 				<Col xs className="w-100 h-15">
-					<input className="m-auto mt-3 mb-3  w-100 h-15" type="text" placeholder="Last Name" />
+					<input className="m-auto mt-3 mb-3  w-100 h-15 rounded" type="text" placeholder="Last Name" />
 				</Col>
 			</Row>
 			<Row>
 				<Col xs className="w-100 h-15">
-					<input className="m-auto mt-3 mb-3  w-100 h-15" type="email" placeholder="Email" />
+					<input className="m-auto mt-3 mb-3  w-100 h-15 rounded" type="email" placeholder="Email" />
 				</Col>
 			</Row>
 			<Row>
 				<Col xs className="w-100 h-15">
-					<input className="m-auto mt-3 mb-3  w-100 h-15" type="password" placeholder="Password" />
+					<input className="m-auto mt-3 mb-3  w-100 h-15 rounded" type="password" placeholder="Password" />
 				</Col>
 			</Row>
 			<Row>
 				<Col xs className="w-100 h-15">
 					<input
-						className="m-auto mt-3 mb-3  w-100 h-15"
+						className="m-auto mt-3 mb-3  w-100 h-15 rounded"
 						type="confirmationPassword"
 						placeholder="Confirmation Password"
 					/>
 				</Col>
 			</Row>
+			<div className="text-end">
+				<Button variant="success" type="submit">
+					Submit
+				</Button>
+			</div>
+			<br />
 			<Link to="/">
 				<Form.Text className="text-start" style={{ marginLeft: "10px" }}>
 					If you do have an account. please click this link to sign in.
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,35 +6,45 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
 	return (
-		<Container>
-			<Row>
-				<Form
-					md={6}
-					style={{ width: "60%", height: "50%", margin: "auto", border: "solid black 1px", margin: "auto" }}
-					className="m-4">
-					<Form.Group className="mb-3" controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
-						<Form.Text className="text-muted">Well never share your email with anyone else.</Form.Text>
-					</Form.Group>
+		<div className="m-auto">
+			<Container>
+				<Row>
+					<Form
+						md={6}
+						style={{
+							width: "60%",
+							height: "50%",
+							margin: "auto",
+							border: "solid black 1px",
+							margin: "auto"
+						}}
+						className="m-4">
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label>Email address</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" />
+							<Form.Text className="text-muted">Well never share your email with anyone else.</Form.Text>
+						</Form.Group>
 
-					<Form.Group className="mb-3" controlId="formBasicPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Password" />
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="formBasicCheckbox">
-						<Form.Check type="checkbox" label="Check me out" />
-					</Form.Group>
-					<Button variant="primary" type="submit" className="mb-3">
-						Submit
-					</Button>
-					<Link to="/signUp">
-						<Form.Text className="text-start" style={{ marginLeft: "10px" }}>
-							If you do not have an account. please click this link to create one
-						</Form.Text>
-					</Link>
-				</Form>
-			</Row>
-		</Container>
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label>Password</Form.Label>
+							<Form.Control type="password" placeholder="Password" />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicCheckbox">
+							<Form.Check type="checkbox" label="Check me out" />
+						</Form.Group>
+						<div className="text-end">
+							<Button variant="success" type="submit" className="mb-3">
+								Submit
+							</Button>
+						</div>
+						<Link to="/signUp">
+							<Form.Text className="text-start" style={{ marginLeft: "10px" }}>
+								If you do not have an account. please click this link to create one
+							</Form.Text>
+						</Link>
+					</Form>
+				</Row>
+			</Container>
+		</div>
 	);
 };
