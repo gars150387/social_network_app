cbe50933856763eda88bb65042c4feeb5ea62ece
gars150387 <gar.santeliz@gmail.com>
2021-11-11 22:15:25 +0000

    email title renamed

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -20,7 +20,7 @@ export const Home = () => {
 						}}
 						className="m-4">
 						<Form.Group className="mb-3" controlId="formBasicEmail">
-							<Form.Label>Email address</Form.Label>
+							<Form.Label>ECheck Email Address</Form.Label>
 							<Form.Control type="email" placeholder="Enter email" />
 							<Form.Text className="text-muted">Well never share your email with anyone else.</Form.Text>
 						</Form.Group>
